f085b461ccb3584d9b5e69d8843d08be5ccd3df7
Cole Thorpen <thorpencole@gmail.com>
2020-10-27 18:38:34 -0700

    Initial Commit

---
+++ pa6-cthorpen/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  pa6-cthorpen
+//
+//  Created by Cole Thorpen on 10/27/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
